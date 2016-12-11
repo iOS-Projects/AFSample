5a8ea8a553c10251d3b8f1c4bb640c48ed858dac
AmrethVC <amrethcheyo@gmail.com>
2016-12-11 16:26:57 +0700

    first commit

---
+++ AFSample/API_Info.swift
@@ -0,0 +1,9 @@
+//
+//  API_Info.swift
+//  AFSample
+//
+//  Created by Amreth on 12/11/16.
+//  Copyright © 2016 Amreth. All rights reserved.
+//
+
+import Foundation
---
+++ AFSample/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  AFSample
+//
+//  Created by Amreth on 12/10/16.
+//  Copyright © 2016 Amreth. All rights reserved.
+//
+
+import Foundation
